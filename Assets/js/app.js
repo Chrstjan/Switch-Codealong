85513709f3a5cc53ee86b8255378e53cb18835ac
Chrstjan <ps5christian@hotmail.com>
2024-02-26 09:52:29 +0100

    Demo of arrays for baliabba

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,15 +1,59 @@
-let value = 100;
+// let value = 100;
 
-switch (value) {
-    case 50:
-        console.log(`Value is: ${value}`);
-        break;
+// switch (value) {
+//     case 50:
+//         console.log(`Value is: ${value}`);
+//         break;
         
-    case 100:
-        console.log(`Value is: ${value}`);
-        break;
+//     case 100:
+//         console.log(`Value is: ${value}`);
+//         break;
 
-    default:
-        console.log("Fortnite Thanos");
-        break;
-}
+//     default:
+//         console.log("Fortnite Thanos");
+//         break;
+// }
+
+let allCategories = [
+    "smartphones",
+    "laptops",
+    "fragrances",
+    "skincare",
+    "groceries",
+    "home-decoration",
+    "furniture",
+    "tops",
+    "womens-dresses",
+    "womens-shoes",
+    "mens-shirts",
+    "mens-shoes",
+    "mens-watches",
+    "womens-watches",
+    "womens-bags",
+    "womens-jewellery",
+    "sunglasses",
+    "automotive",
+    "motorcycle",
+    "lighting"
+  ]
+
+  let eletronicsArray = [];
+  let fortniteArray = [];
+
+  allCategories.forEach(category => {
+    switch(category) {
+        case "smartphones":
+        case "laptops": {
+            eletronicsArray.push(category);
+            break;
+        }
+
+        default: {
+            fortniteArray.push(category);
+            break;
+        }
+    }
+  })
+
+  console.log(eletronicsArray);
+  console.log(fortniteArray);
