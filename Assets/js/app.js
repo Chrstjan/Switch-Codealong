d7ef1c92293cb2139aea40ccfd71c390563b157b
Chrstjan <ps5christian@hotmail.com>
2024-02-26 08:52:40 +0100

    switch

---
+++ Assets/js/app.js
@@ -0,0 +1,15 @@
+let value = 100;
+
+switch (value) {
+    case 50:
+        console.log(`Value is: ${value}`);
+        break;
+        
+    case 100:
+        console.log(`Value is: ${value}`);
+        break;
+
+    default:
+        console.log("Fortnite Thanos");
+        break;
+}
